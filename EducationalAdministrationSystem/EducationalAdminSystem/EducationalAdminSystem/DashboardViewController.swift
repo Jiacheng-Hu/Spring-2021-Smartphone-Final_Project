f5fbd79e53cec45297c5a1af30078962d7c1f9d1
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2021-04-22 16:34:08 +0800

    Checking Dashboard and adding GradeTableViewController

--- EducationalAdministrationSystem/EducationalAdminSystem/EducationalAdminSystem/DashboardViewController.swift
+++ EducationalAdministrationSystem/EducationalAdminSystem/EducationalAdminSystem/DashboardViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 import Firebase
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var imgLogo: UIImageView!
+    @IBOutlet weak var imgEvents: UIImageView!
+    @IBOutlet weak var imgCourse: UIImageView!
+    @IBOutlet weak var imgSchedule: UIImageView!
+    @IBOutlet weak var imgGrade: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imgLogo.image = UIImage(named: "logo")
+        imgEvents.image = UIImage(named: "Events")
+        imgCourse.image = UIImage(named: "Course")
+        imgSchedule.image = UIImage(named: "Schedule")
+        imgGrade.image = UIImage(named: "Grade")
+        
         navigationItem.setHidesBackButton(true, animated: false)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
     @IBAction func logoutAction(_ sender: UIBarButtonItem) {
@@ -28,5 +43,20 @@ class DashboardViewController: UIViewController {
         
     }
     
+    @IBAction func eventsAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "eventsSegue", sender: self)
+    }
+    
+    @IBAction func courseAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "courseSegue", sender: self)
+    }
+    
+    @IBAction func scheduleAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "scheduleSegue", sender: self)
+    }
+    
+    @IBAction func gradeAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "gradeSegue", sender: self)
+    }
     
 }
---
+++ EducationalAdministrationSystem/EducationalAdminSystem/Service/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  EducationalAdminSystem
+//
+//  Created by 胡嘉诚 on 2021/4/22.
+//
+
+import Foundation
+
+let eventsURL: String = "https://demo2247279.mockable.io/events"
+let courseURL: String = "https://demo2247279.mockable.io/course"
+let gradeURL: String = "https://demo2247279.mockable.io/grade"
